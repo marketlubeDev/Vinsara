0a7124bacb8cb4d8a721bdcec9b9d5ed2c3260d4
Market lube <marketlubedevelopers@gmail.com>
2025-07-14 11:20:31 +0530

    cors

--- Vinsara_server/index.js
+++ Vinsara_server/index.js
@@ -24,7 +24,7 @@ app.use(
       "http://localhost:5175",
       "http://localhost:5173",
       "https://vinsara-theta.vercel.app",
-      "https://vinsara-admin.vercel.app/",
+      "https://vinsara-admin.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
