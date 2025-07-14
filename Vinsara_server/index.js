1792041ea385f47bfd100cc0c00fceef2144cd1a
Market lube <marketlubedevelopers@gmail.com>
2025-07-14 11:09:44 +0530

    cors updated

--- Vinsara_server/index.js
+++ Vinsara_server/index.js
@@ -23,6 +23,7 @@ app.use(
       "http://localhost:5174",
       "http://localhost:5175",
       "http://localhost:5173",
+      "https://vinsara-theta.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
