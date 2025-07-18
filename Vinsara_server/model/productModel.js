6f7f7968f4e36c5f69a4b88a9a2cef434f2310fa
Market lube <marketlubedevelopers@gmail.com>
2025-07-18 16:54:45 +0530

    ratings

--- Vinsara_server/model/productModel.js
+++ Vinsara_server/model/productModel.js
@@ -4,24 +4,17 @@ const { Schema } = mongoose;
 const productSchema = new Schema(
   {
     name: { type: String, required: true },
-    // brand: { type: Schema.Types.ObjectId, ref: "Brand" },
     category: { type: Schema.Types.ObjectId, ref: "Category" },
     subcategory: { type: Schema.Types.ObjectId, ref: "SubCategory" },
     variants: [{ type: Schema.Types.ObjectId, ref: "Variant" }],
-    // store: {
-    //   type: Schema.Types.ObjectId,
-    //   ref: "Store",
-    //   required: true,
-    // },
     activeStatus: { type: Boolean, default: true },
-    // References to Variant documents
-    // Fields for non-variant products
-    
     createdBy: { type: Schema.Types.ObjectId, ref: "User" },
     label: { type: Schema.Types.ObjectId, ref: "Label" },
     isDeleted: { type: Boolean, default: false },
     offer: { type: Schema.Types.ObjectId, ref: "Offer" },
     priority: { type: Number, enum: [0, 1], default: 0 },
+    averageRating: { type: Number, default: 0 },
+    totalRatings: { type: Number, default: 0 },
   },
   { timestamps: true }
 );
