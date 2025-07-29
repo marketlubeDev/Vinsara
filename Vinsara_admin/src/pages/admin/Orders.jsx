55d5b76657b167909b2236601b6b36736db5c23d
marketlube <marketlubes@gmail.com>
2025-07-29 12:19:13 +0530

    reduce the size of order items box

--- Vinsara_admin/src/pages/admin/Orders.jsx
+++ Vinsara_admin/src/pages/admin/Orders.jsx
@@ -408,7 +408,7 @@ function Orders() {
 
     return (
       <tr className="bg-white border-b hover:bg-gray-50">
-        <td className="px-6 py-4">
+        <td className="px-6 py-4 w-56">
           <div className="max-h-32 overflow-y-auto">
             {formatProducts(order.products)}
           </div>
@@ -752,7 +752,7 @@ function Orders() {
             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
               <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
-                  <th scope="col" className="px-6 py-3">
+                  <th scope="col" className="px-6 py-3 w-70">
                     Order Items
                   </th>
                   <th scope="col" className="px-6 py-3">
