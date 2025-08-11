04e739a3f9f8f5244a7667ba6f55c411db91d935
Arjunliljo <arjun7180@gmail.com>
2025-08-11 18:55:44 +0530

    SL

--- Vinsara_user/src/components/FacebookLoginButton.jsx
+++ Vinsara_user/src/components/FacebookLoginButton.jsx
@@ -2,6 +2,13 @@ import React, { useEffect } from "react";
 import FacebookLogin from "@greatsumini/react-facebook-login";
 import { useFacebookLogin } from "../hooks/queries/facebookAuth";
 
+// Check if Facebook SDK is loaded
+if (typeof window !== "undefined") {
+  window.fbAsyncInit = function () {
+    console.log("Facebook SDK initialized");
+  };
+}
+
 const FacebookLoginButton = ({
   className = "",
   children,
@@ -23,7 +30,12 @@ const FacebookLoginButton = ({
   });
 
   const handleFacebookResponse = (response) => {
-    console.log(response, "response");
+    console.log("Facebook response received:", response);
+
+    if (response.status === "unknown") {
+      console.error("User cancelled login or did not fully authorize.");
+      return;
+    }
 
     if (response.accessToken) {
       // Extract user info from Facebook response
@@ -65,44 +77,54 @@ const FacebookLoginButton = ({
       fields={fields}
       scope={scope}
       callback={handleFacebookResponse}
+      onFailure={(error) => {
+        console.error("Facebook SDK error:", error);
+        if (onError) onError(error);
+      }}
       disableMobileRedirect={true}
       isMobile={false}
       version="v18.0"
       xfbml={false}
       cookie={false}
-      render={(renderProps) => (
-        <button
-          type="button"
-          onClick={renderProps.onClick}
-          disabled={isLoading || renderProps.isDisabled}
-          className={`facebook-login-button ${className}`}
-        >
-          {isLoading ? (
-            <div className="loading-spinner">
-              <div className="spinner"></div>
-              <span>Signing in...</span>
-            </div>
-          ) : (
-            children || (
-              <>
-                <svg
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"
-                    fill="#1877F2"
-                  />
-                </svg>
-                <span>Continue with Facebook</span>
-              </>
-            )
-          )}
-        </button>
-      )}
+      render={(renderProps) => {
+        console.log("Facebook render props:", renderProps);
+        return (
+          <button
+            type="button"
+            onClick={() => {
+              console.log("Facebook button clicked!");
+              renderProps.onClick();
+            }}
+            disabled={isLoading || renderProps.isDisabled}
+            className={`facebook-login-button ${className}`}
+          >
+            {isLoading ? (
+              <div className="loading-spinner">
+                <div className="spinner"></div>
+                <span>Signing in...</span>
+              </div>
+            ) : (
+              children || (
+                <>
+                  <svg
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"
+                      fill="#1877F2"
+                    />
+                  </svg>
+                  <span>Continue with Facebook</span>
+                </>
+              )
+            )}
+          </button>
+        );
+      }}
     />
   );
 };
